a03427c37d2a9e3ac58827ef5a5e2abec2ab971a
David Ubuntu <lid1355218215@outlook.com>
2020-11-12 18:30:48 -0800

    working on getting releases from local

--- getFiles.py
+++ getFiles.py
@@ -2,32 +2,33 @@ from os import listdir
 from os.path import isfile, join
 import yaml
 
-def GetFilesToSend(path,match):
-    files=dict()
+
+def GetFilesToSend(path, match):
+    files = dict()
     for f in listdir(path):
-        if isfile(join(path,f)):
+        if isfile(join(path, f)):
             if f.find(match) >= 0:
                 if f.find("~") == -1:
-                    files[f]=join(path,f)
+                    files[f] = join(path, f)
     return files
 
 
 def GetAllFilesToSend(args):
     allFiles = []
 
-    dtsiSlavesFile=args.dtsiPath+"slaves.yaml"
+    dtsiSlavesFile = args.dtsiPath+"slaves.yaml"
     allFiles.append(dtsiSlavesFile)
 
     for slave in yaml.load(open(dtsiSlavesFile))['DTSI_CHUNKS']:
-        dtsiFile=GetFilesToSend(args.dtsiPath+"hw/",slave+".")    
+        dtsiFile = GetFilesToSend(args.dtsiPath+"hw/", slave+".")
         for file in dtsiFile:
             allFiles.append(dtsiFile[file])
 
-    tableSlavesFile=args.tablePath+"slaves.yaml"
+    tableSlavesFile = args.tablePath+"slaves.yaml"
     allFiles.append(tableSlavesFile)
 
     tableYAML = yaml.load(open(tableSlavesFile))
-    uploadXMLFileList=[]
+    uploadXMLFileList = []
     for slave in tableYAML['UHAL_MODULES']:
         slave = tableYAML['UHAL_MODULES'][slave]
         if 'XML' in slave:
@@ -36,7 +37,7 @@ def GetAllFilesToSend(args):
                     uploadXMLFileList.append(xmlFile)
     allFiles.extend(uploadXMLFileList)
 
-    bitFiles=GetFilesToSend('bit/','top')
+    bitFiles = GetFilesToSend('bit/', 'top')
     for file in bitFiles:
         allFiles.append(bitFiles[file])
 
